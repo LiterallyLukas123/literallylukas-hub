2ac841dec3cecf4206a99a4c52fbc77ce09933c2
LiterallyLukas <contact@literallylukas.dev>
2026-04-18 17:19:42 +0200

    debug whoami

--- functions/api/whoami.js
+++ functions/api/whoami.js
@@ -1,7 +1,17 @@
 export async function onRequestGet({ request, env }) {
   const email = request.headers.get("cf-access-authenticated-user-email");
   const admin = email && env.ADMIN_EMAIL && email.toLowerCase() === env.ADMIN_EMAIL.toLowerCase();
-  return new Response(JSON.stringify({ admin: !!admin }), {
+
+  const debug = {
+    admin: !!admin,
+    headerEmail: email,
+    envAdminEmail: env.ADMIN_EMAIL,
+    envHasAdminEmail: "ADMIN_EMAIL" in env,
+    envKeys: Object.keys(env),
+    allHeaders: Object.fromEntries([...request.headers.entries()].filter(([k]) => k.startsWith("cf-"))),
+  };
+
+  return new Response(JSON.stringify(debug, null, 2), {
     headers: { "content-type": "application/json" },
   });
 }
